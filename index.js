d4b4967c287ebbaab21b663860bed059a02a339d
Tony <lam_273@hotmail.com>
2023-01-19 15:14:22 -0500

    final

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+let mongoose = require('mongoose');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +12,72 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.use(express.urlencoded({extended: false}));
+mongoose.connect(
+  process.env.URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+let urlSchema = mongoose.Schema({
+  url_original: {
+    type: String,
+    required: true
+  },
+  url_short: {
+    type: Number,
+    unique: true
+  }
+});
+
+let urlModel = mongoose.model('urlModel', urlSchema);
+
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', async (req, res) => {
+  let result = {"original_url": undefined, "short_url": undefined};
+  let invalidUrl = { "error": 'invalid url' }; 
+  let invalidHost = { "error": "invalid Hostname"};
+  let inputUrl = req.body.url;
+  let regex = /^(http|https):\/\/(w{3}\.)*([\w]+\.[\w]+)/;
+
+  if (regex.test(inputUrl) == false) {return res.send(invalidUrl)};
+
+  dns.lookup(inputUrl.match(regex)[3], (error, address, family) => {
+    if (error) {return res.send(invalidHost);}
+  });
+
+  let urlDB = await urlModel.findOne({url_original: inputUrl});
+
+  if (urlDB == '') {
+    let lastIndex = await urlModel.findOne({}).sort({url_short: -1});
+    let entry = new urlModel({
+      url_original: inputUrl,
+      url_short: lastIndex.url_short + 1
+    });
+    entry.save((err, data) => {if(err){return console.error(err)}});
+    urlDB = await urlModel.find({url_original: inputUrl});
+    console.log(urlDB)
+  }
+  result.original_url = urlDB.url_original;
+  result.short_url = urlDB.url_short;
+  res.send(result)
+})
+
+app.get('/api/shorturl/:short', async (req, res) => {
+  let shortParam = req.params.short;
+  let urlDB = await urlModel.findOne({url_short: shortParam});
+  res.redirect(urlDB.url_original);
+})
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
